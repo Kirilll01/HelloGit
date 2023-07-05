6c46e333a53910c03b1db9cdc7373f631eef8344
Kirilll01 <kirill_golovin_01@mail.ru>
2023-07-05 11:19:54 +0500

    new project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите необходимый баланс");
+        int balans = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите необходимый вес");
+        int weight =Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine("Введите необходимую стоимость");
+        int cost = Convert.ToInt32( Console.ReadLine());
+        int sum = weight * cost;
+        balans+= sum;
+        Console.Write("Итоговый баланс равен:_");
+        Console.WriteLine(balans);
+    }
+}
